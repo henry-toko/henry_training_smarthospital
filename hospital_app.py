844f62191915da6e1be8ac277fdd0979bdd22fb5
henry-toko <hendrik@kodingnext.com>
2026-06-03 10:44:56 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import os
+import numpy as np
 
+st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = "🏥", layout="wide")
+
+st.title("Smart Hospital Navigator")
